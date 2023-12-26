3cb27d0c67521e6bdaa8f8c13dadf3d6f4b3762f
jyotirmay <jyotirmay2000gupta@gmail.com>
2023-12-26 17:45:00 +0530

    change in data base name

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 function Connectiontomongoose(){
     mongoose
-      .connect("mongodb://localhost:27017/jyotirmay", {
+      .connect("mongodb://localhost:27017/notebook", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
