e62d3bb653b7c7cc33263f1e7b10a89449371050
jyotirmay <jyotirmay2000gupta@gmail.com>
2023-12-29 21:36:56 +0530

    use of middle-waare, res, req and sava of mongo

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,21 @@
+const user = require("../models/User")
 const express = require('express')
 const router = express.Router()
 
+// through this file we will gonna deal with login and sign up of the user
 router.get('/',(req,res)=>{
     
-    res.json([])
+    // res.json([])
+    //res we use to send things 
+    // req.body will be assessible only through middleware
+    console.log(req.body)
+    res.send(req.body)
+    const userdata = user(req.body)
+    // broadly by using user(req.body) we are sending the data into the schema which we made and after checking the data we are storing it in const userdata
+    userdata.save()
+    // this save function which led to the save of the data in mongo  db
 })
 
 module.exports = router
+
+// Note : password shouldnot be store in the plain text
