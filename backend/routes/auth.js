c2e52d69152f99b8bf34f680416c6ba9f9fe52eb
jyotirmay <jyotirmay2000gupta@gmail.com>
2023-12-30 19:02:13 +0530

    Express validation code

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,22 +1,33 @@
-const user = require("../models/User")
-const express = require('express')
-const router = express.Router()
+const user = require("../models/User");
+const express = require("express");
+// import { body, matchedData } from 'express-validator';
+const { body, validationResult } = require("express-validator");
+const router = express.Router();
 
 // through this file we will gonna deal with login and sign up of the user
-router.post('/',(req,res)=>{
-    
+router.post(
+  "/",
+  [body("name","Enter a valid name").isLength({min:4}), body("email","Enter a valid Email").isEmail(), body("password","Password must be of atleast 6 characters").isLength({ min: 6 })],
+  (req, res) => {
     // res.json([])
-    //res we use to send things 
+    //res we use to send things
     // req.body will be assessible only through middleware
-    console.log(req.body)
-    res.send(req.body)
-    const userdata = user(req.body)
-    // broadly by using user(req.body) we are sending the data into the schema which we made and checking it then, we are storing it into the variable which we have 
-    userdata.save()
-    // this save function which led to the save of the data in mongo  db
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     
-})
 
-module.exports = router
+    res.send(req.body);
+    console.log(req.body)
+    // const userdata = user(req.body);
+    // broadly by using user(req.body) we are sending the data into the schema which we made and checking it then, we are storing it into the variable which we have
+    // userdata.save();
+    // this save function which led to the save of the data in mongo  db
+  }
+);
+
+module.exports = router;
 
 // Note : password shouldnot be store in the plain text
+// Note : body name , email and password these codes belongs to the user validation format which we want as to be , it will keep track of the format 
