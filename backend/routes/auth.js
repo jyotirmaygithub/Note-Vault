8c2050fb2ce7e5068cbd779b05bb8258300fea26
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-02 18:32:38 +0530

    removing, adding comments and change of router name to /createuser

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,11 @@
 const user = require("../models/User");
 const express = require("express");
-// import { body, matchedData } from 'express-validator';
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
-// through this file we will gonna deal with login and sign up of the user
+// we will gonna create the user POST : /api/auth/createuser : No login required
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid name").isLength({ min: 4 }),
     body("email", "Enter a valid Email").isEmail(),
@@ -15,15 +14,14 @@ router.post(
     }),
   ],
   async (req, res) => {
-    // res.json([])
-    //res we use to send things
-    // req.body will be assessible only through middleware
     try {
+      // below code checks for validation errors using express-validator and sends a response with error details if any validation errors are found. 
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
 
+      ///below function is sending the data into userschema and then storing it in mongodb
       const newUser = await user.create({
         name: req.body.name,
         password: req.body.password,
@@ -32,20 +30,14 @@ router.post(
       res.json(newUser);
       console.log(newUser)
     } catch (error) {
+      // throw errors related to the schema
       console.log(error);
       res.json(error);
     }
-    /// through below function we are sending the data into userschema and then storing it in mongodb
-    // res.send(req.body);
-    // console.log(req.body)
-    // const userdata = user(req.body);
-    // broadly by using user(req.body) we are sending the data into the schema which we made and checking it then, we are storing it into the variable which we have
-    // userdata.save();
-    // this save function which led to the save of the data in mongo  db
   }
 );
 
 module.exports = router;
 
 // Note : password shouldnot be store in the plain text
-// Note : body name , email and password these codes belongs to the user validation format which we want as to be , it will keep track of the format
+
