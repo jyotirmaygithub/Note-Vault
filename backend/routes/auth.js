55f1d96376a3bf1d01dac6c3718e9554af081afe
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-02 17:45:50 +0530

    refactor user validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,19 +7,37 @@ const router = express.Router();
 // through this file we will gonna deal with login and sign up of the user
 router.post(
   "/",
-  [body("name","Enter a valid name").isLength({min:4}), body("email","Enter a valid Email").isEmail(), body("password","Password must be of atleast 6 characters").isLength({ min: 6 })],
-  (req, res) => {
+  [
+    body("name", "Enter a valid name").isLength({ min: 4 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password must be of atleast 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
     // res.json([])
     //res we use to send things
     // req.body will be assessible only through middleware
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-    res.send(req.body);
-    console.log(req.body)
+      const newUser = await user.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      });
+      res.json(newUser);
+      console.log(newUser)
+    } catch (error) {
+      console.log(error);
+      res.json(error);
+    }
+    /// through below function we are sending the data into userschema and then storing it in mongodb
+    // res.send(req.body);
+    // console.log(req.body)
     // const userdata = user(req.body);
     // broadly by using user(req.body) we are sending the data into the schema which we made and checking it then, we are storing it into the variable which we have
     // userdata.save();
@@ -30,4 +48,4 @@ router.post(
 module.exports = router;
 
 // Note : password shouldnot be store in the plain text
-// Note : body name , email and password these codes belongs to the user validation format which we want as to be , it will keep track of the format 
+// Note : body name , email and password these codes belongs to the user validation format which we want as to be , it will keep track of the format
