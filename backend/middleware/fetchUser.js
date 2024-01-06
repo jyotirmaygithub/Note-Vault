42b7d043932191e4d5ccca62fdcc5b088a360e07
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-06 17:35:32 +0530

    Feat : code for middleware functioning

--- backend/middleware/fetchUser.js
+++ backend/middleware/fetchUser.js
@@ -1,2 +1,22 @@
-// we are creating a middleware to fetch user id 
 var jwt = require("jsonwebtoken");
+// we are creating a middleware to fetch user id 
+// middle-ware is a function 
+
+const JWT_secret = "mynameisanthoneygonservice";
+
+function fetchuser(req,res,next){
+    const token = req.header("auth-token")
+    if(!token){
+        return res.status(401).send({msg:"No Token Provided"})
+    }
+    try {
+        const data = jwt.verify(token , JWT_secret)
+        req.user = data;
+        next()
+    } catch (error) {
+        return res.status(401).send({msg : 'Invalid Token'});
+    }
+    
+}
+
+module.exports = fetchuser
