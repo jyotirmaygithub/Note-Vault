1c021a1fc4c9fa4a905cd30d1a9b44090a74c8a4
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-10 22:56:32 +0530

    Refactoring of code

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,11 +8,11 @@ const fetchuser = require("../middleware/fetchUser");
 
 const JWT_secret = "mynameisanthoneygonservice";
 
-//Router 1 : creating an user account POST : /api/auth/createuser
+//ROUTE 1 : creating an user account POST : /api/auth/createuser
 router.post(
   "/createuser",
   [
-    // First check : where entered credentials are checking by the validation function.
+    // CHECK 1 : where entered credentials are checking by the validation function.
     body("name", "Enter a valid name").isLength({ min: 4 }),
     body("email", "Enter a valid Email").isEmail(),
     body("password", "Password must be of atleast 6 characters").isLength({
@@ -20,26 +20,26 @@ router.post(
     }),
   ],
   async (req, res) => {
-    // Result of validation : if error occured at validation so it will return type and place of error
+    // VALIDATION RESULTS : if error occured at validation so it will return type and place of error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     try {
-      // Second check : we dont want two or more user of same email id (we are checking it with existing app database).
+      // CHECK 2 : we dont want two or more user of same email id (we are checking it with existing app database).
       let newUser = await user.findOne({ email: req.body.email });
       if (newUser) {
         console.log("user already exist");
         return res.status(400).json("User already exists");
       }
-      // Note :Gonna create a document of new user after passing all checks with some additional security.
+      // NOTE :Gonna create a document of new user after passing all checks with some additional security.
       // salt and hash we are using to ensure better security to the user.
       // gensalt() function creates a unique set of number or letters which add-up to the actual password
       // then hash is created from that salt and our password.
       const salt = await bcrypt.genSalt(10);
       // secpass is storing a hashed password which comprises of user's password and salt.
       const secPass = await bcrypt.hash(req.body.password, salt);
-      ///Creation of a document : User entered data is being send to the user defined schema to store it into the database.
+      // DOCUMENT CREATION : User entered data is being send to the user defined schema to store it into the database.
       newUser = await user.create({
         name: req.body.name,
         password: secPass,
@@ -63,11 +63,10 @@ router.post(
   }
 );
 
-// Router 2 : Authenticate a user using : POST /api/auth/login . login required (No sign up required)
-
+// ROUTE 2 : Authenticate a user using : POST /api/auth/login . login required (No sign up required)
 router.post(
   "/login",
-  //Requesting : user need to enter email and password to enter into the app. By clicking on the submit btn user will gonna create a request to verify and data will go along.
+  //REQUESTING : user need to enter email and password to enter into the app. By clicking on the submit btn user will gonna create a request to verify and data will go along.
   [
     body("email", "Enter a valid Email").isEmail(),
     body("password", "Password cannot be blank").exists(),
@@ -77,16 +76,16 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    // Extraction : extracting pass and email to verify it with the existing mongodb database.
+    // EXTRACTION : extracting pass and email to verify it with the existing mongodb database.
     const { email, password } = req.body;
     try {
-      // First check : Email
+      // CHECK 1 : Email
       // user.findOne({ email }), it queries the MongoDB database to find a document where the "email" field matches the provided email. If there's a match, it retrieves the entire document associated with that email
       let existingUser = await user.findOne({ email });
       if (!existingUser) {
         return res.status(401).json({ msg: "Invalid Credentials" });
       }
-      // Second Check : Password
+      // CHECK 2 : Password
       // entered password and stored password should match to verify user
       const existingPassword = await bcrypt.compare(
         password,
@@ -101,7 +100,7 @@ router.post(
           id: existingUser.id,
         },
       };
-      // Here : we creating a token by using the user id (stored in data-base) and jwt-secret-key which only backend knows
+      // HERE : we creating a token by using the user id (stored in data-base) and jwt-secret-key which only backend knows
       const auth_token = jwt.sign(data, JWT_secret);
       console.log(auth_token);
       // Sending token to the client
@@ -113,14 +112,14 @@ router.post(
   }
 );
 
-// Router 3 : Logged in user details: POST : "api/auth/getuser".
+// ROUTE 3 : Logged in user details: POST : "api/auth/getuser".
 // fetchuser function : is a middleware acting as a bridge to make id avaiable, to fetch entire document from the database.
 router.post("/getuser",fetchuser, async (req, res) => {
   try {
     
     const userId = req.userDetails;
-    //findById : method to find document.
-    //select : method used to specify which fields should be included or excluded in the query result.
+    //FINDBYID : method to find document.
+    //SELECT : method used to specify which fields should be included or excluded in the query result.
     const User = await user.findById(userId).select("-password");
     res.send(User);
   } catch (error) {
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const userNotes = require("../models/Notes");
+const userNote = require("../models/Notes");
 const fetchuser = require("../middleware/fetchUser");
 const { body, validationResult } = require("express-validator");
 
-//Route 1 : Fetch all the notes by using --->> GET : /api/notes/fetchusernotes
+//Route 1 : Fetch existing notes by using --->> GET : /api/notes/fetchusernote
 // fetchuser --> middleware.
-router.get("/fetchusernotes", fetchuser, async (req, res) => {
+router.get("/fetchusernote", fetchuser, async (req, res) => {
   try {
-    // use of middlewar : getting id of the user.
-    const fetchingNOtes = await userNotes.find({ user: req.userDetails });
-    res.send(fetchingNOtes);
+    // MIDDLEWARE USE : getting id of the user.
+    const fetchingNote = await userNote.find({ user: req.userDetails });
+    res.send(fetchingNote);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("error related to fetch notes ");
   }
 });
 
-// ROUTE 2 : create notes -->> POST : /api/notes/addnotes
+// ROUTE 2 : create note -->> POST : /api/notes/addnote
 router.post(
-  "/addnotes",
+  "/addnote",
   [
-    // First check :
+    // CHECK 1 : validation checks.
     body("title", "Enter a valid title").isLength({ min: 3 }),
     body("description", "Description atleast must be of 5 characters").isLength(
       { min: 5 }
@@ -30,20 +30,20 @@ router.post(
   ],
   fetchuser,
   async (req, res) => {
-    //Second check :  Validation check.
+    //CHECK 2 : Express Validation check.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     try {
-      // Document Creation : through method and values
-      const creatednote = await userNotes.create({
+      // DOCUMENT CREATION : through methods and values.
+      const createdNote = await userNote.create({
         user: req.userDetails,
         title: req.body.title,
         description: req.body.description,
         tag: req.body.tag,
       });
-      res.send(creatednote);
+      res.send(createdNote);
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal server error");
@@ -51,24 +51,24 @@ router.post(
   }
 );
 
-// ROUTE 3 : Update existing note -->> PUT : /api/notes/updatenotes
-router.put("/updatenotes/:id", fetchuser, async (req, res) => {
-  // create a newnote object to update the existing one.
-  const { title, description, tag } = req.body; // destruturing
-  const newNote = {};
-  if (title) {
-    newNote.title = title;
-  }
-  if (description) {
-    newNote.description = description;
-  }
-  if (tag) {
-    newNote.tag = tag;
-  }
-  // Find a note and then update it after some checks.
+// ROUTE 3 : Update existing note -->> PUT : /api/notes/updatenote
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
   try {
-    // CHECK 1 : finding a note (or document) by using that note id (giving it into route).
-    let foundNote = await userNotes.findById(req.params.id);
+    // create a newnote object to update the existing one.
+    const { title, description, tag } = req.body; // destruturing
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+    // Finding a note to update it after some checks.
+    // CHECK 1 : finding a note by using document id (giving it into route).
+    let foundNote = await userNote.findById(req.params.id);
     if (!foundNote) {
       return res.status(404).send("The note is not available");
     }
@@ -77,7 +77,7 @@ router.put("/updatenotes/:id", fetchuser, async (req, res) => {
       return res.status(401).send("You are not authorized to");
     }
     // UPDATE : Note (or document) with new values by using mongoose function.
-    await userNotes.findByIdAndUpdate(
+    await userNote.findByIdAndUpdate(
       req.params.id,
       { $set: newNote },
       { new: true }
@@ -94,7 +94,7 @@ router.delete("/deletenote/:id", fetchuser, async (req, res) => {
   // Find a note and then delete it after some checks.
   try {
     // CHECK 1 : finding a note (or document) by using that note id (giving it into route).
-    let foundNote = await userNotes.findById(req.params.id);
+    let foundNote = await userNote.findById(req.params.id);
     if (!foundNote) {
       return res.status(404).send("The note is not available");
     }
@@ -102,8 +102,8 @@ router.delete("/deletenote/:id", fetchuser, async (req, res) => {
     if (foundNote.user.toString() !== req.userDetails) {
       return res.status(401).send("You are not authorized to");
     }
-    // UPDATE : Note (or document) with new values by using mongoose function.
-    await userNotes.findByIdAndDelete(req.params.id);
+    // UPDATE : Note with new values by using mongoose function.
+    await userNote.findByIdAndDelete(req.params.id);
     res.json({ Success: "Note has been deleted ", foundNote: foundNote });
   } catch (error) {
     console.error(error.message);
