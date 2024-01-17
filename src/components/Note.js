c351f89a9565cc8fc0095ae54accf2356c99589a
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-17 15:26:58 +0530

    Refactor code to use API calls for CRUD operations.

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import {UserNotes} from "../Context/NoteState"
-import ShowNote from './ShowNote';
+import React, { useEffect } from "react";
+import { UserNotes } from "../Context/NoteState";
+import ShowNote from "./ShowNote";
 
 export default function Note() {
-    const {notes} = UserNotes();
+  const { notes, fetchAllNotes } = UserNotes();
+  useEffect(() => {
+    fetchAllNotes();
+  }, []);
   return (
-    <div className='flex space-x-6'>
-      {notes.map((data)=>{
-        return (
-          <ShowNote note={data}/>
-        );
-      })}
+    <div className="flex space-x-6">
+      {/* {notes.map((data) => {
+        return <ShowNote note={data} />;
+      })} */}
     </div>
-  )
+  );
 }
