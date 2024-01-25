95de497f3a6b57da2f7c195802dbadf047addb48
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-25 11:21:57 +0530

    Feat : addnote connection done with server

--- src/Context/NoteContext.js
+++ src/Context/NoteContext.js
@@ -77,9 +77,8 @@ export function NoteContextFun(props) {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
-          "auth-token":
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuZXdVc2VyIjp7ImlkIjoiNjU5NTU4ODU0NmFlZjEyMDc1MzVhNjdhIn0sImlhdCI6MTcwNDg3NzE4OH0.0UfBodadf9kZNLpeexYY6nrvHOixiSAtUDLnBmUzqqQ",
-        },
+          "auth-token": getCookie("auth_token"),
+           },
         body: JSON.stringify({ title, description, tag }),
       });
       fetchAllNotes();
