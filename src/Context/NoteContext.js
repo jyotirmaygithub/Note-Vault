bd49cb4cb377132442beada1e35eaaab21b42cb1
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-25 11:05:40 +0530

    Feat : fetch user notes connection done

--- src/Context/NoteContext.js
+++ src/Context/NoteContext.js
@@ -1,27 +1,30 @@
 import React, { useState, useContext, createContext } from "react";
 
-
 const noteContext = createContext();
 
 const dev_URL = process.env.REACT_APP_DEV_URL;
 
 function getCookie(cookieName) {
-  console.log(cookieName)
-  const cookies = document.cookie.split('.');
-  console.log("cookies = ",cookies)
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
-    console.log("cookie trimming =" ,cookie)
+  const cookies = document.cookie;
+  const cookieArray = cookies.split('; ');
+  console.log("cookieArray = ",cookieArray)
+
+  for (const cookie of cookieArray) {
     if (cookie.startsWith(`${cookieName}=`)) {
-      return cookie.substring(cookieName.length + 1);
+      // Extract and return the value of the cookie
+      const cookieValue = cookie.split('=')[1];
+      console.log("cookieValue = ",cookieValue)
+      return cookieValue;
     }
   }
+
   return null;
 }
-
 export function NoteContextFun(props) {
   // Use "props" instead of "{ Children }"
   const [notes, setnotes] = useState([]);
+  const temp = getCookie("auth_token")
+  console.log("belongs to temp = " ,temp)
 
   // Fetching, adding, updating and deleting will be done through API calls.
   // API call 1: To fetch all existing notes.
@@ -31,9 +34,9 @@ export function NoteContextFun(props) {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          // Need to change : from the retriving from the cookie or session storage 
-          "auth-token": getCookie("auth_token")
-         },
+          // Need to change : from the retriving from the cookie or session storage
+          "auth-token": getCookie("auth_token"),
+        },
       });
 
       if (!response.ok) {
@@ -41,13 +44,12 @@ export function NoteContextFun(props) {
       }
 
       const jsonData = await response.json();
-      console.log("existing data of the user " ,jsonData);
+      console.log("existing data of the user ", jsonData);
       setnotes(jsonData);
     } catch (error) {
       console.error("Error fetching notes:", error);
     }
   }
-
 
   // API call 3 : To delete a note.
   async function handleDeleteNote(id) {
@@ -69,8 +71,8 @@ export function NoteContextFun(props) {
     }
   }
   // API call 4 : To edit exiting note.
-  async function handleEditNote(id,title,description,tag) {
-    console.log("id at the context side = " , id)
+  async function handleEditNote(id, title, description, tag) {
+    console.log("id at the context side = ", id);
     try {
       const response = await fetch(`${dev_URL}/api/notes/updatenote/${id}`, {
         method: "PUT",
