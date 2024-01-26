876c6fb2972eff82574230679f27fa4b96ee4263
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-26 18:56:25 +0530

    sign up material ui

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,37 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Form, Button } from "react-bootstrap";
 import Alert from "../components/Alerts";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export default function SignUp() {
+  const defaultTheme = createTheme();
+  function Copyright(props) {
+    return (
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        NoteVault {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
+
   const [combinedState, setCombinedState] = useState({
     username: "",
     email: "",
@@ -17,7 +45,7 @@ export default function SignUp() {
     setCombinedState({ ...combinedState, [e.target.name]: e.target.value });
   }
 
-  async function handlesubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     console.log(combinedState);
     await handleCreateUser(
@@ -55,17 +83,19 @@ export default function SignUp() {
 
       const userAuth_Token = await response.json();
 
-      if (userAuth_Token && userAuth_Token.auth_token) {    
+      if (userAuth_Token && userAuth_Token.auth_token) {
         // Set the cookie with an expiration time
         const expirationDate = new Date();
         expirationDate.setDate(expirationDate.getDate() + 7); // Set to expire in 7 days
-        document.cookie = `auth_token=${userAuth_Token.auth_token}; expires=${expirationDate.toUTCString()}; path=/`;
-      
+        document.cookie = `auth_token=${
+          userAuth_Token.auth_token
+        }; expires=${expirationDate.toUTCString()}; path=/`;
+
         setDetails({ look: "success", des: "valid credentials" });
         setAlertState(true);
         alertRemoval();
         setTimeout(() => {
-          Navigation(`/`);
+          Navigation(`/home`);
         }, 2500);
       }
     } catch (error) {
@@ -74,51 +104,86 @@ export default function SignUp() {
   }
 
   return (
-    <div className="sign-in__wrapper">
+    <ThemeProvider theme={defaultTheme}>
       <div>
-      {alertState && <Alert looks={details.look} des={details.des} />}
+        {alertState && <Alert looks={details.look} des={details.des} />}
       </div>
-      <div className="sign-in__backdrop"></div>
-      {/* Form */}
-      <Form className="shadow p-4 bg-white rounded" onSubmit={handlesubmit}>
-        <div className="h4 mb-2 text-center">Create an account</div>
-        <Form.Group className="mb-2" controlId="username">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            name="username"
-            placeholder="Username"
-            onChange={onchange}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-2" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="text"
-            name="email"
-            placeholder="Email"
-            onChange={onchange}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-2" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={onchange}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-2" controlId="checkbox">
-          <Form.Check type="checkbox" label="Remember me" />
-        </Form.Group>
-        <Button className="w-100" variant="primary" type="submit">
-          Sign In
-        </Button>
-      </Form>
-    </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "black" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="family-name"
+                  onChange={onchange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={onchange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={onchange}
+                />
+              </Grid>
+            </Grid>
+            <Button
+            className="bg-black"
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container className="text-black cursor-pointer underline flex justify-end">
+              <Grid item>
+                  Already have an account? Sign in
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
   );
 }
