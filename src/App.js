a33231e5bb2282bc64d0d85cc4dc5c1e29affcfb
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-27 18:05:47 +0530

    Addition : new routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import React from "react";
-import Navbar from "./components/CustomNavbar"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import {NoteContextFun} from "./Context/NoteContext"
+import Navbar from "./components/CustomNavbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { NoteContextFun } from "./Context/NoteContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
 import EnterBtn from "./pages/EnterBtn";
 import ProgressCircle from "./pages/ProgressCircle";
-
+import AddNote from "./pages/AddNote";
+import ExistingNotes from "./pages/ExistingNotes";
 
 export default function App() {
   return (
-    <div className="App"> 
-    <NoteContextFun>
+    <div className="App">
+      <NoteContextFun>
         <Router>
           {/* <Navbar /> */}
           <Routes>
-            <Route exact path="/" element={<EnterBtn/>}/>
-            <Route exact path="/home" element={<Home/>}/>
+            <Route exact path="/" element={<EnterBtn />} />
+            <Route exact path="/create-notes" element={<AddNote />} />
+            <Route path="/existing-notes" element={<ExistingNotes />} />
             <Route exact path="/about" element={<About />} />
             <Route exact path="/contact" element={<Contact />} />
             <Route exact path="/login" element={<LogIn />} />
-            <Route exact path="/login/fetchingdata" element={<ProgressCircle />} />
+            <Route
+              exact
+              path="/login/fetchingdata"
+              element={<ProgressCircle />}
+            />
             <Route exact path="/signup" element={<SignUp />} />
           </Routes>
         </Router>
-      </NoteContextFun>       
+      </NoteContextFun>
     </div>
   );
 }
