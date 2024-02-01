c241602cead1de6971b673634bb94a24eabe1b54
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-02-01 11:31:20 +0530

    Feat : routes in switch button

--- src/Navbar/SwitchBtn.js
+++ src/Navbar/SwitchBtn.js
@@ -1,21 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function SwitchBtn() {
   const userLocation = useLocation();
+  const navigate = useNavigate();
   const [userSpot, setUserSpot] = useState("Add Note");
 
   useEffect(() => {
     if (userLocation.pathname === "/existing-notes") {
-      setUserSpot('Create Note');
+      setUserSpot("Create Note");
     } else if (userLocation.pathname === "/create-notes") {
-      setUserSpot('Available Notes');
-    } 
+      setUserSpot("Available Notes");
+    }
   }, [userLocation.pathname]);
 
+  function handleClick() {
+    if (userLocation.pathname === "/existing-notes") {
+      navigate("/create-notes");
+    } else if (userLocation.pathname === "/create-notes") {
+      navigate("/fetchingdata");
+    }
+  }
   return (
     <div>
-      <button className='bg-white text-black px-4 py-2 rounded-full'>{userSpot}</button>
+      <Button
+        onClick={handleClick}
+        className="bg-white text-black px-4 py-2 rounded-full"
+      >
+        {userSpot}
+      </Button>
     </div>
   );
 }
