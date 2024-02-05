a602438a37db58b43e3fd7298668868eda371a99
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-02-05 15:11:19 +0530

    Feat : new context for username

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import { NoteContextFun } from "./Context/NoteContext";
-import {ContextFun} from "./Context/Context"
+import { ContextFun } from "./Context/Context";
+import { UserNameContextFunc } from "./Context/UserNameContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
@@ -17,23 +18,42 @@ export default function App() {
     <div className="App">
       <NoteContextFun>
         <ContextFun>
-          <Router>
-            {/* <Navabar /> */}
-            <Routes>
-              <Route exact path="/" element={<EnterBtn />} />
-              <Route exact path="/create-notes" element={<><Navabar /><AddNote /></>} />
-              <Route path="/existing-notes" element={<><Navabar /><ExistingNotes /></>} />
-              <Route exact path="/about" element={<About />} />
-              <Route exact path="/contact" element={<Contact />} />
-              <Route exact path="/login" element={<LogIn />} />
-              <Route
-                exact
-                path="/fetchingdata"
-                element={<ProgressCircle />}
-              />
-              <Route exact path="/signup" element={<SignUp />} />
-            </Routes>
-          </Router>
+          <UserNameContextFunc>
+            <Router>
+              {/* <Navabar /> */}
+              <Routes>
+                <Route exact path="/" element={<EnterBtn />} />
+                <Route
+                  exact
+                  path="/create-notes"
+                  element={
+                    <>
+                      <Navabar />
+                      <AddNote />
+                    </>
+                  }
+                />
+                <Route
+                  path="/existing-notes"
+                  element={
+                    <>
+                      <Navabar />
+                      <ExistingNotes />
+                    </>
+                  }
+                />
+                <Route exact path="/about" element={<About />} />
+                <Route exact path="/contact" element={<Contact />} />
+                <Route exact path="/login" element={<LogIn />} />
+                <Route
+                  exact
+                  path="/fetchingdata"
+                  element={<ProgressCircle />}
+                />
+                <Route exact path="/signup" element={<SignUp />} />
+              </Routes>
+            </Router>
+          </UserNameContextFunc>
         </ContextFun>
       </NoteContextFun>
     </div>
