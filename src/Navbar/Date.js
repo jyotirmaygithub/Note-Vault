9efae94018a50b0c7c154115c185f9c80bf9d2df
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-02-07 12:02:06 +0530

    Feat : compoent for date

---
+++ src/Navbar/Date.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography, Box } from '@mui/material';
+
+export default function DateComponent() {
+  const currentDate = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = currentDate.toLocaleDateString('en-US', options);
+
+  return (
+    <Box>
+      <Typography className='text-white' variant="p">Day & Date</Typography>
+      <Typography className='text-white' variant="subtitle1">{formattedDate}</Typography>
+    </Box>
+  );
+}
